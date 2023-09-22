a50719a52f36dee3d7fda0f81551804d4cde7852
haruka <solap5hyk237@gmail.com>
2023-09-22 20:56:08 +0900

    パス変数でcornerを指定

--- src/main/java/com/example/crudapi/controller/LocationSearchController.java
+++ src/main/java/com/example/crudapi/controller/LocationSearchController.java
@@ -9,9 +9,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -28,8 +28,8 @@ public class LocationSearchController {
         this.locationSearchService = locationSearchService;
     }
 
-    @GetMapping("/locations")
-    public LocationResponse findByCorner(@RequestParam("corner") @NotBlank String corner) {
+    @GetMapping("/locations/{corner}")
+    public LocationResponse findByCorner(@PathVariable("corner") @NotBlank String corner) {
         LocationDto locationDto = locationSearchService.findByCorner(corner);
         return new LocationResponse(locationDto);
     }
@@ -44,8 +44,8 @@ public class LocationSearchController {
         return ResponseEntity.created(url).body(Map.of("message", "location successfully created"));
     }
 
-    @PatchMapping("/locations")
-    public ResponseEntity<Map<String, String>> updateLocation(@RequestParam("corner") String corner, @RequestBody @Validated LocationForm form) {
+    @PatchMapping("/locations/{corner}")
+    public ResponseEntity<Map<String, String>> updateLocation(@PathVariable("corner") String corner, @RequestBody @Validated LocationForm form) {
         locationSearchService.updateLocation(form.convertToLocationDto());
         return ResponseEntity.ok(Map.of("message", "location successfully updated"));
     }
