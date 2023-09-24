ee893b27fe87c5d55fa1db6aac722b3e0121a167
haruka <solap5hyk237@gmail.com>
2023-09-24 16:17:47 +0900

    Exception_HandlerのHttpStatusを変更

--- src/main/java/com/example/crudapi/controller/LocationSearchExceptionHandler.java
+++ src/main/java/com/example/crudapi/controller/LocationSearchExceptionHandler.java
@@ -1,5 +1,7 @@
-package com.example.crudapi.exception;
+package com.example.crudapi.controller;
 
+import com.example.crudapi.exception.DuplicateCornerException;
+import com.example.crudapi.exception.NoCornerFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
@@ -13,7 +15,7 @@ import java.util.Map;
 @RestControllerAdvice
 public class LocationSearchExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<Map<String, String>> badRequestException(ConstraintViolationException e, HttpServletRequest request) {
+    public ResponseEntity<Map<String, String>> handleBadRequestException(ConstraintViolationException e, HttpServletRequest request) {
         Map<String, String> body = Map.of(
                 "status", String.valueOf(HttpStatus.BAD_REQUEST.value()),
                 "error", HttpStatus.BAD_REQUEST.getReasonPhrase(),
@@ -25,20 +27,20 @@ public class LocationSearchExceptionHandler extends ResponseEntityExceptionHandl
     @ExceptionHandler(NoCornerFoundException.class)
     public ResponseEntity<Map<String, String>> handleNoCornerFoundException(NoCornerFoundException e, HttpServletRequest request) {
         Map<String, String> body = Map.of(
-                "status", String.valueOf(HttpStatus.BAD_REQUEST.value()),
-                "error", HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                "status", String.valueOf(HttpStatus.NOT_FOUND.value()),
+                "error", HttpStatus.NOT_FOUND.getReasonPhrase(),
                 "message", e.getMessage(),
                 "path", request.getRequestURI());
-        return new ResponseEntity(body, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity(body, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(DuplicateCornerException.class)
     public ResponseEntity<Map<String, String>> handleDuplicationCornerException(DuplicateCornerException e, HttpServletRequest request) {
         Map<String, String> body = Map.of(
-                "status", String.valueOf(HttpStatus.BAD_REQUEST.value()),
-                "error", HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                "status", String.valueOf(HttpStatus.CONFLICT.value()),
+                "error", HttpStatus.CONFLICT.getReasonPhrase(),
                 "message", e.getMessage(),
                 "path", request.getRequestURI());
-        return new ResponseEntity(body, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity(body, HttpStatus.CONFLICT);
     }
 }
